3493c2adc748c1d8b6383efcb4899d191aac3b34
mauriceRogers <scsurfermaurice@gmail.com>
2022-03-08 03:24:07 -0800

    form submission functionality
    submit button makes form invisible and 'message p' visible

--- survey0/script.js
+++ survey0/script.js
@@ -1,9 +1,11 @@
-const submitBtn = document.querySelector("#submit");
-const container = document.querySelector(".container")
-const message = document.querySelector(".message")
-
+const submitBtn = document.querySelector(".submit");
+const container = document.querySelector(".container");
+const message = document.querySelector(".message");
+const copyright = document.querySelector(".copy-right");
 
 submitBtn.addEventListener('click', function(){
-    container.style.display = none;
-    message.style.display = block;
+    event.preventDefault();
+    container.style.display = "none";
+    message.style.display = "block";
+    copyright.style.display = "none";
 })
