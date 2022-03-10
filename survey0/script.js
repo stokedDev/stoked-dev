768ad7256b84e1f33d3816d33112d26d48d5843e
mauriceRogers <scsurfermaurice@gmail.com>
2022-03-10 15:18:58 -0800

    properly place portfolio link with submit btn JS
    portfolio link centered at bottom upon submit click

--- survey0/script.js
+++ survey0/script.js
@@ -2,10 +2,12 @@ const submitBtn = document.querySelector(".submit");
 const container = document.querySelector(".container");
 const message = document.querySelector(".message");
 const copyright = document.querySelector(".copy-right");
+const seePortfolio = document.querySelector(".ftr-nav-link");
 
 submitBtn.addEventListener('click', function(){
     event.preventDefault();
     container.style.display = "none";
     message.style.display = "block";
     copyright.style.display = "none";
+    seePortfolio.classList.add("linkToBottom")
 })
