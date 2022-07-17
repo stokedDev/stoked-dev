fde7aa129c346001253fb56a7ae48146df800b72
mauriceRogers <scsurfermaurice@gmail.com>
2022-07-17 16:08:44 -0700

    make navbar buttons work
    when btn clicked page scrolls to destination

---
+++ product-landing/product-landing.js
@@ -0,0 +1,23 @@
+const [aboutBtn, locationsBtn, pricingBtn] = 
+[
+    document.querySelector('.about'),
+    document.querySelector('.locations'),
+    document.querySelector('.pricing')
+]
+
+const [aboutH, locationsH, pricingH] =
+[
+    document.querySelector('.about-title'),
+    document.querySelector('.locations-title'),
+    document.querySelector('.pricing-title')
+]
+
+function goToSection(header){
+    header.scrollIntoView();
+}
+function activateBtn(btn, header){
+btn.addEventListener("click", () => goToSection(header));
+}
+activateBtn(aboutBtn, aboutH);
+activateBtn(locationsBtn, locationsH);
+activateBtn(pricingBtn, pricingH);
