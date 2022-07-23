21877e99eb9987075c9314f8d470120fb122bb24
mauriceRogers <scsurfermaurice@gmail.com>
2022-07-22 18:04:05 -0700

    Feature: mobile nav with animated hamburger icon
    Adapt mobile menu code (from my last few commits) for my portfolio webpage. Put all CSS and JS for portfolio webpage index.html in folder. Improve letterspacing in welcome section.

---
+++ index-css-and-js/script.js
@@ -0,0 +1,121 @@
+const slides = document.querySelectorAll('.carousel-item');
+let slidePosition = 0;
+function hideAllSlides(){
+    for(const slide of slides){
+        slide.classList.remove('carousel-item-visible') && slide.classList.add('carousel-item-hidden');
+    }
+}
+
+/* nav div classes buttons */
+const aboutBtn = document.querySelector(".aboutBtn");
+const projectsBtn = document.querySelectorAll(".projectsBtn");
+const contactBtn = document.querySelector(".contactBtn");
+/* nav div classes buttons end here*/
+
+/* sI means section intro */
+const [ sI1, sI2, sI3 ] = [document.querySelector('h4'), document.querySelector('.projects-title'),
+document.querySelector('.contact-description')];
+
+const header = document.querySelector('header');
+const headerDiv = document.querySelector('.header');
+const [topBun,inBuns,bottomBun] = 
+[
+    document.querySelector('.top-bun'),
+    document.querySelector('.in-buns'),
+    document.querySelector('.bottom-bun')
+];
+const mobileMenu = document.querySelector('.mobile-menu');
+
+function hamburgerBack(){
+    topBun.classList.remove('topBunToX');
+    inBuns.classList.remove('inBunsToX');
+    bottomBun.classList.remove('bottomBunToX');
+    mobileMenu.style.marginLeft = "";
+    topBun.classList.add('topBunBack');
+    inBuns.classList.add('inBunsBack');
+    bottomBun.classList.add('bottomBunBack');
+    setTimeout(() => {
+    topBun.classList.remove('topBunBack');
+    inBuns.classList.remove('inBunsBack');
+    bottomBun.classList.remove('bottomBunBack');
+    }, 501)
+}
+
+function removeHeaderPopupClasses(){
+    header.classList.remove('header-popup');
+    headerDiv.classList.remove('header-div-popup'); 
+}
+function goToSection(sectionIntro, slidePosition){
+    switch (sectionIntro){
+        case sI1:
+            removeHeaderPopupClasses()
+            hamburgerBack()
+            hideAllSlides();
+        break;
+        case sI2:
+            removeHeaderPopupClasses()
+            hamburgerBack()
+            hideAllSlides();
+        break;
+        case sI3:
+            removeHeaderPopupClasses()
+            hamburgerBack()
+            hideAllSlides();
+    }
+    slides[slidePosition].classList.add('carousel-item-visible');
+    sectionIntro.scrollIntoView();
+}
+function activateBtn(btn, sectionIntro, slidePosition){
+    btn.addEventListener("click", () => goToSection(sectionIntro, slidePosition));
+    }
+    mobileMenu.addEventListener("click", function(){
+        if (!header.classList.contains('header-popup')){
+            topBun.classList.remove('topBunBack');
+            inBuns.classList.remove('inBunsBack');
+            bottomBun.classList.remove('bottomBunBack');
+            mobileMenu.style.marginLeft = "-18px";
+            topBun.classList.add('topBunToX');
+            inBuns.classList.add('inBunsToX');
+            bottomBun.classList.add('bottomBunToX');
+            header.classList.add('header-popup');
+            headerDiv.classList.add('header-div-popup');
+            
+        } else {
+            header.classList.remove('header-popup');
+            headerDiv.classList.remove('header-div-popup');
+            hamburgerBack()    
+        }
+    
+    });
+
+    function isInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    
+        );
+    }
+    function isNotInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.bottom <= 0
+        );
+    }
+    let lapTopScreen = window.matchMedia("(min-width: 1024px)");
+
+    document.addEventListener('scroll', function () {
+        if(isNotInViewport(contactBtn) && !lapTopScreen.matches) mobileMenu.style.display = "flex";
+        if(isInViewport(contactBtn) &&
+         !header.classList.contains('header-popup')) mobileMenu.style.display = "none";
+    }, {
+        passive: true
+    });
+
+
+    activateBtn(aboutBtn, sI1, 0);
+    activateBtn(projectsBtn[0],sI2, 1);
+    activateBtn(projectsBtn[1],sI2, 1);
+    activateBtn(contactBtn, sI3, 2);
