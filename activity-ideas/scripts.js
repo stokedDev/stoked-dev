0aba41c55a28f27561db189733fcd9370a587a0f
mauriceRogers <scsurfermaurice@gmail.com>
2022-08-15 17:45:28 -0700

    Create error handling for fetch
    Http fetch can't be utilized with my https website so I just added a dynamic error message using the fetch method catch callback

--- activity-ideas/scripts.js
+++ activity-ideas/scripts.js
@@ -1,18 +1,22 @@
-const idea = document.querySelector('.idea')
-const btn = document.querySelector('#btn')
+const idea = document.querySelector('.idea');
+const btn = document.querySelector('#btn');
 btn.addEventListener('click', function(){
     fetch('http://www.boredapi.com/api/activity/')
 .then(response => response.json())
 .then(data => {
     idea.textContent = data.activity;
-    idea.style.animationName = 'growShrink'   
+    idea.style.animationName = 'growShrink';   
 })
-})
+.catch(error => {
+    idea.textContent = `HTTP Error: ${error}`;
+    idea.style.animationName = 'growShrink'; 
+});
+});
 
 btn.addEventListener('click', function(){
     btn.style.animationName = 'shadowFade';
-})
+});
 btn.addEventListener('animationend', function(){
     btn.style.animationName = '';
     idea.style.animationName = '';
-})
+});
