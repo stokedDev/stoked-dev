39858bb31f1427fd46bd938a0dbb1db5ccf96e1d
mauriceRogers <scsurfermaurice@gmail.com>
2022-08-28 15:40:57 -0700

    Add response to old browser lack of support for web workers

--- my-worker-example/script.js
+++ my-worker-example/script.js
@@ -3,22 +3,19 @@
     document.querySelector('.current-count'),
     document.querySelector('.num')];
 
-
-/* 
-Thought Journey for this project->
-every 1000 nums counted by worker, put nums in grid, then request 1000 more from worker and repeat this loop
-*/
-const worker = new Worker('worker.js');
-    function countHigh(){
-            const numVal = numInput.value
-            worker.postMessage({
-                command: 'count',
-                numVal
-            })
+    if(window.Worker){
+        const worker = new Worker('worker.js');
+        function countHigh(){
+                const numVal = numInput.value
+                worker.postMessage({
+                    command: 'count',
+                    numVal
+                })
+        }
+        countBtn.addEventListener("click", () => countHigh())
+        worker.addEventListener('message', (message) => {
+            count.textContent = `${message.data} counted.`
+        })
+    } else {
+        count.textContent = "Your browser doesn't support web workers."
     }
-    countBtn.addEventListener("click", () => countHigh())
-    worker.addEventListener('message', (message) => {
-        count.textContent = `${message.data} counted.`
-    })
-    
-
