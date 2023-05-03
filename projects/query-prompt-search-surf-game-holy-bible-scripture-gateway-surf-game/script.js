77708434e67ca6af4504af29956954894189cf4a
Maurice Rogers <scsurfermaurice@gmail.com>
2023-05-03 11:33:06 -0700

    Commit To Push GitHub Sabotaged Files
    Add Commit Push index-css-and-js/index.css, index.html, ( projects/surf-game/index.html,scripts.js,styles.css );

--- projects/query-prompt-search-surf-game-holy-bible-scripture-gateway-surf-game/script.js
+++ projects/query-prompt-search-surf-game-holy-bible-scripture-gateway-surf-game/script.js
@@ -1,11 +1,71 @@
-/* Allow player to start game after entering username. Use localStorage to keep track of user scores and display their history after each wave(wave lasts for limited time to maximize incentive to play) Different ranks can be achieved with various levels of final wave scores. In gameplay, depending on players position on wave, there are different opportunities to perform maneuvers. For instance, if player is at bottom and gets to top of wave outside barrel in limited time, aerial is performed if certain key is pressed within limited time before or having reached top of wave. Using this same logic will enable constant stream of: encouraging messages about tricks, points, combos. Each trick will be essential to creating unique combos. */
+/* Our Holy God Of Our Lord Jesus Christ Is LORD */
+/* Input Interactivity For Mobile */
+/* Specifically Select HTML CSS Classes */
+/* Thank You And/Or Praise You Our Holy God Of Our Lord Jesus Christ Who Is LORD */
+/* Thank You Nico's Sister CoCo ( Collette? ) Virostko For Your Idea About Surf Game > Violent Game */
+/*  We Did It! */
+/* 
+
+I Am Maurice Scott Rogers;
+
+I Am A Citizen Of The United States Of The United States Of America;
+
+I Am A Citizen Of The Heavens And The Earth
+
+Holy God Breath Breathed Scripture
+
+Genesis Chapter One Verse One "In The Beginning God Created The Heavens And The Earth";
+
+https://www.BibleGateWay.com
+
+jajajaja
+
+hahahaha
+
+0 :-)
+1 :-)
+2 :-)
+3 :-)
+4 :-)
+5 :-)
+6 :-)
+7 :-)
+
+Our Holy God Of Our Lord Jesus Christ Is LORD
+
+https://www.ESV.com
+https://www.ESV.org
+https://www.BibleGateWay.com
+https://www.HolyBible.com
+https://www.CrossWay.com
+
+"" !== ""? "" // !== ; && "" !== " " && [] !== [] ;
+
+[] !== [] ;
+
+https://www.StokedDev.com // StokedDev is about the first 12+ results on of Brave Search, Google Search, Bing Search, etc;
+StokedDev is about the first 12 + results on of from Brave Search , Google Search , Bing Search , and other query receivers;
+StokedDev , We Like And Love God The First Coder
+StokedDev , We Have Infinite Wants And/Or Needs For Our Holy Father Of Our Lord Jesus Christ Who Is LORD And Father Our House; 
+
+/* Allow player to start game after entering username. Use localStorage to keep track of user scores and display their history after each wave(wave lasts for limited time to maximize incentive to play) Different ranks can be achieved with various levels of final wave scores. In gameplay, depending on players position on wave, there are different opportunities to perform maneuvers. For instance, if player is at bottom and gets to top of wave outside barrel in limited time, aerial is performed if certain key is pressed within limited time before or having reached top of wave. Using this same logic will enable constant stream of: encouraging messages about tricks, points, combos. Each trick will be essential to creating unique combos. 
+
+*//* // ACCESS CONSTANT VARIABLES BY ARRAY ELEMENT INDEX ACCESSOR FOR EXAMPLE [0] */
+const MOBILE_CONTROLLER = document.getElementsByClassName(".mobile-controller");/* // ACCESS CONSTANT VARIABLES BY ARRAY ELEMENT INDEX ACCESSOR FOR EXAMPLE [0] */
+const UP = document.getElementsByClassName(".up");/* // ACCESS CONSTANT VARIABLES BY ARRAY ELEMENT INDEX ACCESSOR FOR EXAMPLE [0] */
+const LEFT = document.getElementsByClassName(".left"); /* // ACCESS CONSTANT VARIABLES BY ARRAY ELEMENT INDEX ACCESSOR FOR EXAMPLE [0] */ 
+const RIGHT = document.getElementsByClassName(".right");/* // ACCESS CONSTANT VARIABLES BY ARRAY ELEMENT INDEX ACCESSOR FOR EXAMPLE [0] */
+const DOWN = document.getElementsByClassName(".down") /* // ACCESS CONSTANT VARIABLES BY ARRAY ELEMENT INDEX ACCESSOR FOR EXAMPLE [0] */
+console.log(MOBILE_CONTROLLER);
+console.log(UP);
+
 const grid = document.querySelector(".grid");
 const scoreDisplay = document.querySelector("#score");
 const messageDisplay = document.querySelector("#message");
-messageDisplay.textContent = "Enjoy the ride...";
+messageDisplay.textContent = "Enjoy the ride..."
 const width = 28;
 let score = 0;
-let squares = [];
+let squares = []
 /* sun = 0 // sun could be another element placed with negative margin
 sky = 1
 ocean = 2
@@ -42,13 +102,8 @@ let environment = [
     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
     6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-];
-const [MOBILE_CONTROLLER, UP, LEFT, RIGHT, DOWN] = 
-[document.querySelector(".mobile-controller"), 
-document.querySelector(".mobile-controller-up"), 
-document.querySelector(".mobile-controller-left"),
-document.querySelector(".mobile-controller-right"), 
-document.querySelector(".mobile-controller-down")];
+]
+
 /* sun = 0 // sun could be another element placed with negative margin
 sky = 1
 ocean = 2
@@ -130,61 +185,7 @@ function control(e){
 }
 document.addEventListener('keydown', control, {
     passive: true
-});
-/* MOBILE_CONTROLLER JS Below */
-function mobileControl(e){
-    if(squares[surferCurrentIndex].classList.contains('barrel')){
-        squares[surferCurrentIndex].classList.remove('tubed-surfer')
-    }
-    squares[surferCurrentIndex].classList.remove('surfer')
-    switch(e.target){
-        case DOWN:
-            if( 
-                !squares[surferCurrentIndex + width].classList.contains('ocean') &&
-                surferCurrentIndex + width < width * width)
-                surferCurrentIndex += width;            
-        break;
-        case UP:
-            if(
-                !squares[surferCurrentIndex - width].classList.contains('ocean') &&
-                surferCurrentIndex - width >= 0)
-                 surferCurrentIndex -= width;
-                 
-                 
-            
-        break;
-        case LEFT:
-            if(
-                !squares[surferCurrentIndex - 1].classList.contains('ocean') &&
-                surferCurrentIndex % width !== 0) 
-                surferCurrentIndex -= 1;
-                
-                
-        break;
-        case RIGHT:
-            if(
-                !squares[surferCurrentIndex + 1].classList.contains('ocean') &&
-                surferCurrentIndex % width < width - 1)
-                surferCurrentIndex += 1;
-                
-        break;
-    }
-    squares[surferCurrentIndex].classList.add('surfer')
-    if(squares[surferCurrentIndex].classList.contains('barrel')){
-        squares[surferCurrentIndex].classList.remove('surfer')
-        squares[surferCurrentIndex].classList.add('tubed-surfer')
-    }
-    if(squares[surferCurrentIndex].classList.contains('white-wash')){
-        squares[surferCurrentIndex].classList.remove('surfer')
-        squares[surferCurrentIndex].classList.add('white-wash')
-    }
-    ridingMessage()    
-};
-
-document.addEventListener('mousedown' || 'touchstart', mobileControl, {
-    passive: true
-});
-/* MOBILE_CONTROLLER JS Above */
+})
 let k = environment.indexOf(5)
 
 function whiteWash(){
@@ -220,3 +221,5 @@ function ridingMessage(){
         messageDisplay.style.color = 'black'
 }
 }
+
+/* :-) */ /* :-) is allowed in VScode */
