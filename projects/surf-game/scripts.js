43d97cedff57fa8e1da613335892512cdce4055b
Maurice Rogers <scsurfermaurice@gmail.com>
2023-05-17 15:59:35 -0700

    Update Surf Game JS
    Make top comment multi-line, remove useless comments, improve indentation, add semi-colons, and make surfer always disappear in white wash.

--- projects/surf-game/scripts.js
+++ projects/surf-game/scripts.js
@@ -1,4 +1,9 @@
-/* Allow player to start game after entering username. Use localStorage to keep track of user scores and display their history after each wave(wave lasts for limited time to maximize incentive to play) Different ranks can be achieved with various levels of final wave scores. In gameplay, depending on players position on wave, there are different opportunities to perform maneuvers. For instance, if player is at bottom and gets to top of wave outside barrel in limited time, aerial is performed if certain key is pressed within limited time before or having reached top of wave. Using this same logic will enable constant stream of: encouraging messages about tricks, points, combos. Each trick will be essential to creating unique combos. */
+/* Allow player to start game after entering username. 
+Use localStorage to keep track of user scores and display their history after each wave(wave lasts for limited time to maximize incentive to play) Different ranks can be achieved with various levels of final wave scores. 
+In gameplay, depending on players position on wave, there are different opportunities to perform maneuvers. 
+For instance, if player is at bottom and gets to top of wave outside barrel in limited time, aerial is performed if certain key is pressed within limited time before or having reached top of wave. 
+Using this same logic will enable constant stream of: encouraging messages about tricks, points, combos. 
+Each trick will be essential to creating unique combos. */
 const grid = document.querySelector(".grid");
 const scoreDisplay = document.querySelector("#score");
 const messageDisplay = document.querySelector("#message");
@@ -6,11 +11,11 @@ messageDisplay.textContent = "Enjoy the ride...";
 const width = 28;
 let score = 0;
 let squares = [];
-/* sun = 0 // sun could be another element placed with negative margin
+/* 
 sky = 1
 ocean = 2
 wave face = 3
-barrel = 4 // change surfer color to alpha when in barrel
+barrel = 4
 white wash = 5
 beach = 6 */
 let environment = [
@@ -49,18 +54,18 @@ document.querySelector(".mobile-controller-up"),
 document.querySelector(".mobile-controller-left"),
 document.querySelector(".mobile-controller-right"), 
 document.querySelector(".mobile-controller-down")];
-/* sun = 0 // sun could be another element placed with negative margin
+/* 
 sky = 1
 ocean = 2
 wave-face = 3
-barrel = 4 // change surfer color to alpha when in barrel
+barrel = 4
 white-wash = 5
 beach = 6 */
 function createLevel(){
     for(let i = 0; i < environment.length; i++){
-        const square = document.createElement('div')
-        grid.appendChild(square)
-        squares.push(square)
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        squares.push(square);
         if(environment[i] === 1){
             squares[i].classList.add('sky')
         } else if(environment[i] === 2){
@@ -76,15 +81,16 @@ function createLevel(){
         }
     }
 }
-createLevel()
+createLevel();
 
 let surferCurrentIndex = 256;
-squares[surferCurrentIndex].classList.add('surfer')
+squares[surferCurrentIndex].classList.add('surfer');
+
 function control(e){
     if(squares[surferCurrentIndex].classList.contains('barrel')){
-        squares[surferCurrentIndex].classList.remove('tubed-surfer')
+        squares[surferCurrentIndex].classList.remove('tubed-surfer');
     }
-    squares[surferCurrentIndex].classList.remove('surfer')
+    squares[surferCurrentIndex].classList.remove('surfer');
     switch(e.key){
         case "ArrowDown":
             if( 
@@ -97,36 +103,30 @@ function control(e){
                 !squares[surferCurrentIndex - width].classList.contains('ocean') &&
                 surferCurrentIndex - width >= 0)
                  surferCurrentIndex -= width;
-                 
-                 
-            
         break;
         case "ArrowLeft":
             if(
                 !squares[surferCurrentIndex - 1].classList.contains('ocean') &&
                 surferCurrentIndex % width !== 0) 
-                surferCurrentIndex -= 1;
-                
-                
+                surferCurrentIndex -= 1; 
         break;
         case "ArrowRight":
             if(
                 !squares[surferCurrentIndex + 1].classList.contains('ocean') &&
                 surferCurrentIndex % width < width - 1)
-                surferCurrentIndex += 1;
-                
+                surferCurrentIndex += 1;       
         break;
     }
-    squares[surferCurrentIndex].classList.add('surfer')
+    squares[surferCurrentIndex].classList.add('surfer');
     if(squares[surferCurrentIndex].classList.contains('barrel')){
-        squares[surferCurrentIndex].classList.remove('surfer')
-        squares[surferCurrentIndex].classList.add('tubed-surfer')
+        squares[surferCurrentIndex].classList.remove('surfer');
+        squares[surferCurrentIndex].classList.add('tubed-surfer');
     }
     if(squares[surferCurrentIndex].classList.contains('white-wash')){
-        squares[surferCurrentIndex].classList.remove('surfer')
-        squares[surferCurrentIndex].classList.add('white-wash')
+        squares[surferCurrentIndex].classList.remove('tubed-surfer');
+        squares[surferCurrentIndex].classList.remove('surfer');
     }
-    ridingMessage()    
+    ridingMessage();    
 }
 document.addEventListener('keydown', control, {
     passive: true
@@ -134,9 +134,9 @@ document.addEventListener('keydown', control, {
 /* MOBILE_CONTROLLER JS Below */
 function mobileControl(e){
     if(squares[surferCurrentIndex].classList.contains('barrel')){
-        squares[surferCurrentIndex].classList.remove('tubed-surfer')
+        squares[surferCurrentIndex].classList.remove('tubed-surfer');
     }
-    squares[surferCurrentIndex].classList.remove('surfer')
+    squares[surferCurrentIndex].classList.remove('surfer');
     switch(e.target){
         case DOWN:
             if( 
@@ -148,58 +148,55 @@ function mobileControl(e){
             if(
                 !squares[surferCurrentIndex - width].classList.contains('ocean') &&
                 surferCurrentIndex - width >= 0)
-                 surferCurrentIndex -= width;
-                 
-                 
-            
+                 surferCurrentIndex -= width; 
         break;
         case LEFT:
             if(
                 !squares[surferCurrentIndex - 1].classList.contains('ocean') &&
                 surferCurrentIndex % width !== 0) 
-                surferCurrentIndex -= 1;
-                
-                
+                surferCurrentIndex -= 1;       
         break;
         case RIGHT:
             if(
                 !squares[surferCurrentIndex + 1].classList.contains('ocean') &&
                 surferCurrentIndex % width < width - 1)
-                surferCurrentIndex += 1;
-                
+                surferCurrentIndex += 1;       
         break;
     }
-    squares[surferCurrentIndex].classList.add('surfer')
+    squares[surferCurrentIndex].classList.add('surfer');
     if(squares[surferCurrentIndex].classList.contains('barrel')){
-        squares[surferCurrentIndex].classList.remove('surfer')
-        squares[surferCurrentIndex].classList.add('tubed-surfer')
+        squares[surferCurrentIndex].classList.remove('surfer');
+        squares[surferCurrentIndex].classList.add('tubed-surfer');
     }
     if(squares[surferCurrentIndex].classList.contains('white-wash')){
-        squares[surferCurrentIndex].classList.remove('surfer')
-        squares[surferCurrentIndex].classList.add('white-wash')
+        squares[surferCurrentIndex].classList.remove('tubed-surfer');
+        squares[surferCurrentIndex].classList.remove('surfer');
     }
-    ridingMessage()    
+    ridingMessage();    
 };
 
 document.addEventListener('mousedown' || 'touchstart', mobileControl, {
     passive: true
 });
 /* MOBILE_CONTROLLER JS Above */
-let k = environment.indexOf(5)
 
 function whiteWash(){
     setInterval(function(){
-        squares[476].classList.add('white-wash')
-        squares[505].classList.add('white-wash')
-        squares[534].classList.add('white-wash')   
+        if(squares[surferCurrentIndex].classList.contains('white-wash')){
+            squares[surferCurrentIndex].classList.remove('tubed-surfer');
+            squares[surferCurrentIndex].classList.remove('surfer');
+        }
+        squares[476].classList.add('white-wash');
+        squares[505].classList.add('white-wash'); 
+        squares[534].classList.add('white-wash');  
     }, 200)
     setInterval(function(){
-        squares[476].classList.remove('white-wash')
-        squares[505].classList.remove('white-wash')
-        squares[534].classList.remove('white-wash')   
+        squares[476].classList.remove('white-wash');
+        squares[505].classList.remove('white-wash');
+        squares[534].classList.remove('white-wash');   
     }, 400)
 }
-whiteWash()
+whiteWash();
     
 function getTubePoints(){
     setInterval(function(){
@@ -209,14 +206,14 @@ function getTubePoints(){
         }
     }, 200)    
 }
-getTubePoints()
+getTubePoints();
 
 function ridingMessage(){
     if(squares[surferCurrentIndex].classList.contains('barrel') || squares[surferCurrentIndex].classList.contains('white-wash')){
-        messageDisplay.textContent = "You're getting tubed!"
-        messageDisplay.style.color = 'white'
+        messageDisplay.textContent = "You're getting tubed!";
+        messageDisplay.style.color = 'white';
     } else if(squares[surferCurrentIndex].classList.contains('wave-face')){
         messageDisplay.textContent = 'Enjoy the ride...';
-        messageDisplay.style.color = 'black'
+        messageDisplay.style.color = 'black';
 }
 }
