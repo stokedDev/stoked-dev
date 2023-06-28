64376c4e07c8640529a70c18354e102c1ebcc725
Maurice Rogers <scsurfermaurice@gmail.com>
2023-06-28 11:44:04 -0700

    Update Blackjack JS:Add Cards Animation JS
    Add JS to coordinate with CSS and HTML for picked cards animation. Have messageDisplay for card picking.

--- projects/blackjack-game/scripts.js
+++ projects/blackjack-game/scripts.js
@@ -1,5 +1,8 @@
 const messageDisplay = document.querySelector('#message');
-const cardsDisplay = document.querySelector("#cards");
+const newCardDisplay = document.querySelector(".new-card");
+const card1Display = document.querySelector(".card-1");
+const card2Display = document.querySelector(".card-2");
+const cardsDisplay = document.querySelector(".cards");
 const sumDisplay = document.querySelector('#sum');
 let sum = 0;
 let message = "";
@@ -17,20 +20,46 @@ function getRandomCard(){
     }
 }
 function startGame(){
+    messageDisplay.textContent = '';
+    isAlive = false;
+    let initial2cardsAnimated = false;
+    card1Display.textContent = '';
+    card2Display.textContent = '';
+    newCardDisplay.textContent = '';
+    cardsDisplay.textContent = '';
+    sum = 0;
+    sumDisplay.textContent = 'Sum:';
+    cards = [];
     isAlive = true;
     hasBlackjack = false;
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
-    sum = firstCard + secondCard;
-    cards = [firstCard, secondCard]
-    renderGame()
+    cards = [firstCard, secondCard];
+    card1Display.textContent = `${cards[0]}`;
+    card1Display.classList.add('new-card-animation');
+    messageDisplay.textContent = 'Picking cards...';
+            if(initial2cardsAnimated === false){
+            card1Display.addEventListener('animationend', () => {
+            card1Display.classList.remove('new-card-animation');
+            card1Display.textContent = "";
+            cardsDisplay.textContent = `${cards[0]}`;
+            sumDisplay.textContent = `Sum: ${cards[0]}`;
+            card2Display.textContent = `${cards[1]}`;
+            card2Display.classList.add('new-card-animation');
+        });
+            card2Display.addEventListener('animationend', () => {
+            card2Display.classList.remove('new-card-animation');
+            card2Display.textContent = '';
+            cardsDisplay.textContent = `${cards.join(' ')}`;
+            sum = cards.reduce((acc, el) => acc + el,0);
+            sumDisplay.textContent = `Sum: ${sum}`;
+            initial2cardsAnimated = true;
+            renderGame();
+       });};
 }
 function renderGame(){
-    cardsDisplay.textContent = "Cards: "
-    for(let i = 0; i < cards.length; i++){
-        cardsDisplay.textContent += `${cards[i]} `
-    }
-    sumDisplay.textContent = "Sum: " + sum;
+    sum = cards.reduce((acc, el) => acc + el,0);
+    sumDisplay.textContent = `Sum: ${sum}`;
     if(sum < 21){
         message = "Do you want to pick a new card?";
     } else if(sum === 21){
@@ -46,10 +75,19 @@ function renderGame(){
 }
 
 function newCard(){
+    messageDisplay.textContent = 'Picking card...';
+    let newCardAnimationDone = false;
     if(isAlive === true && hasBlackjack === false){
-        let card = getRandomCard()
-        sum += card;
-        cards.push(card)
-        renderGame()
-    }
+        let card = getRandomCard();
+        cards.push(card);
+        newCardDisplay.textContent = `${cards[cards.length - 1]}`;
+        newCardDisplay.classList.add('new-card-animation');
+        newCardDisplay.addEventListener('animationend', () => {
+            newCardDisplay.textContent = '';
+            newCardDisplay.classList.remove('new-card-animation');
+            cardsDisplay.textContent = `${cards.join(' ')} `;
+            newCardAnimationDone = true; 
+            renderGame(); 
+        });
+    };
 }
