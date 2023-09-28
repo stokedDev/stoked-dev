fbf42f88224a071ef277bf8670a236486f0315a2
Maurice Rogers <scsurfermaurice@gmail.com>
2023-09-28 10:55:27 -0700

    Scroll to funMoney,comment,up weeks
    Smooth scroll funMoney into view,comment about fun money,correct weeks number,add event.preventDefault() semicolon

--- projects/funmoneyapp/script.js
+++ projects/funmoneyapp/script.js
@@ -1,23 +1,24 @@
 let submitBtn = document.querySelector('#submit');
 let total = document.querySelector('#total');
 let totalM = document.querySelector('#total-m');
-let p = document.querySelector('#p');
+let funMoney = document.querySelector('#fun-money');
 let reset = document.querySelector('#reset');
 let form = document.querySelector('#form');
 // have voice recognition technology for option of ease of use
 function calculateBtn(event){
-    event.preventDefault()
+    event.preventDefault();
     let hourlyWage = document.querySelector('#wage-input').value;
     let weeklyWorkDays = document.querySelector('#wwd').value;
     let mle = document.querySelector('#mle-input').value;
     let workHours = document.querySelector('#work-hours').value;
-    let yearlyFunnyMoney = ((hourlyWage * workHours * weeklyWorkDays) * (4.29 * 12)) - (mle * 12);
+    let yearlyFunnyMoney = ((hourlyWage * workHours * weeklyWorkDays) * (4.34 * 12)) - (mle * 12);
     let monthlyFunnyMoney = yearlyFunnyMoney / 12;
     yearlyFunnyMoney = yearlyFunnyMoney.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
     monthlyFunnyMoney = monthlyFunnyMoney.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-    total.textContent = yearlyFunnyMoney;
-    totalM.textContent = monthlyFunnyMoney;
-    p.style.display = 'block';
+    total.textContent = yearlyFunnyMoney; /* yearly part of fun money results paragraph */
+    totalM.textContent = monthlyFunnyMoney; /* monthly part of fun money results paragraph */
+    funMoney.style.display = 'block';
+    funMoney.scrollIntoView(behavior = "smooth");
 }
 
 form.addEventListener('submit', calculateBtn);
